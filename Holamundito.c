40920dd28e690bd1281b60c0a1fb7e93a2cfcd9b
Darwin <93103712+darwin712@users.noreply.github.com>
2024-09-10 09:36:50 -0700

    Update Holamundito.c
    Cositas

--- Holamundito.c
+++ Holamundito.c
@@ -1,1 +1,6 @@
-blaba
+#include <stdio.h>
+
+int main(){
+  printf("Integrantes: David Rey Ordorica Ruiz, Andre Neftali Trujillo Vega, Marco Antonio Hernandez Vazquez, Ernesto Cota Cazares, Santiago Emanuel Gaxiola Davalos");
+  return 0;
+}
